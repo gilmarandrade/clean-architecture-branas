254b975e7c9c1fb942d0614e825b8985c5d48c08
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-27 10:58:22 -0300

    chores [backend]: runing server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,21 @@
-console.log('main')
+import express from 'express'
+import cors from 'cors'
+
+async function main() {
+    const app = express()
+    app.use(cors())
+    app.get('/', function (req, res) {
+        console.log('ok')
+        res.json('ok')
+    })
+    app.get('/products', function (req, res) {
+        res.json([
+            { productId: 1, description: 'A', price: 100 },
+            { productId: 2, description: 'B', price: 200 },
+            { productId: 3, description: 'C', price: 400 },
+        ])
+    })
+    app.listen(3000, () => console.log('listening on port 3000'))
+}
+
+main()
