eb024d8e4e8c5b42893a1df5794224187b702062
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-27 10:39:19 -0300

    chores [backend]: created

--- frontend/src/components/__tests__/App.test.ts
+++ frontend/src/components/__tests__/App.test.ts
@@ -7,6 +7,11 @@ import App from '@/App.vue'
 describe('', () => {
   it('renders properly', () => {
     const wrapper = mount(App)
-    expect(wrapper.get('h1').text()).toBe('fullcycle')
+    expect(wrapper.findAll('.product-description').at(0)?.text()).toBe('A')
+    expect(wrapper.findAll('.product-price').at(0)?.text()).toBe('100')
+    expect(wrapper.findAll('.product-description').at(1)?.text()).toBe('B')
+    expect(wrapper.findAll('.product-price').at(1)?.text()).toBe('200')
+    expect(wrapper.findAll('.product-description').at(2)?.text()).toBe('C')
+    expect(wrapper.findAll('.product-price').at(2)?.text()).toBe('400')
   })
 })
